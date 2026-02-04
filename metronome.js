cc8a1b9708c4cb20e616c0af295cbbc55068f153
Dajmen Graus <dahgraus2002@gmail.com>
2026-02-04 12:40:31 +0100

    Add button functionality to start and stop the metronome

--- metronome.js
+++ metronome.js
@@ -1,18 +1,34 @@
+const btnStart = document.getElementById("btn-start");
+const btnStop = document.getElementById("btn-stop");
+const beatAccent = document.getElementById("beat-accent");
+
 let bpm = 100;
 let beatsPerBar = 4;
 let beatUnit = 4;
 
 let beat = 0;
+let intervalId = null;
 const interval = (60 / bpm) * 1000;
 
-let isRunning = false;
+function start() {
+    if (intervalId !== null) return;
 
-const start = setInterval(() => {
-    beat = (beat % beatsPerBar) + 1;
-    
-    if (beat === 1) {
-        //accented beat
-    } else {
-        //beat
-    }
-}, interval)
+    intervalId = setInterval(() => {
+        beat = (beat % beatsPerBar) + 1;
+        if (beat === 1) {
+            //accented beat
+            console.log("TICK");
+        } else {
+            console.log("tick");
+        }
+    }, interval)
+}
+
+function stop() {
+    clearInterval(intervalId);
+    intervalId = null;
+    beat = 0;
+}
+
+btnStart.addEventListener("click", start);
+btnStop.addEventListener("click", stop)
