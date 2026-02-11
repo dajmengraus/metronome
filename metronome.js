3e5aa9f53eb67257b2ee1d126f0720056e172acd
Dajmen Graus <2062141graus@zuyd.nl>
2026-02-11 22:25:55 +0100

    Add BPM adjustment

--- metronome.js
+++ metronome.js
@@ -2,6 +2,11 @@ const btnStart = document.getElementById("btn-start");
 const btnStop = document.getElementById("btn-stop");
 const beatAccent = document.getElementById("beat-accent");
 const beats = document.getElementById("beats");
+const btnDecreaseBpm = document.getElementById("decrease-bpm")
+const btnIncreaseBpm = document.getElementById("increase-bpm")
+const btnDecreaseFiveBpm = document.getElementById("decrease-five-bpm")
+const btnIncreaseFiveBpm = document.getElementById("increase-five-bpm")
+const bpmText = document.getElementById("bpm");
 
 const beatAudio = new Audio("audio/beat.mp3")
 const beatAccentAudio = new Audio("audio/beat-accent.mp3")
@@ -12,6 +17,36 @@ let beatUnit = 4;
 
 let beat = 0;
 let intervalId = null;
+
+function increaseBpm(incr) {
+    if (intervalId) {    
+        stop()
+        bpm += incr;
+        bpmText.innerText = bpm;
+        start();
+    } else {
+        bpm += incr;
+        bpmText.innerText = bpm;
+    }
+}
+
+function decreaseBpm(decr) {
+    if (decr === 1 && bpm === 1) {
+        return;
+    }
+    if (decr === 5 && bpm === 5) {
+        return;
+    }
+    if (intervalId) {
+        stop();
+        bpm -= decr;
+        bpmText.innerText = bpm;
+        start();
+    } else {
+        bpm -= decr;
+        bpmText.innerText = bpm;
+    }
+}
 
 function getInterval() {
     return (60 / bpm) * 1000;
@@ -51,5 +86,11 @@ function stop() {
     beat = 0;
 }
 
+
 btnStart.addEventListener("click", start);
 btnStop.addEventListener("click", stop);
+
+btnDecreaseBpm.addEventListener("click", () => decreaseBpm(1));
+btnIncreaseBpm.addEventListener("click", () => increaseBpm(1));
+btnDecreaseFiveBpm.addEventListener("click", () => decreaseBpm(5));
+btnIncreaseFiveBpm.addEventListener("click", () => increaseBpm(5));
