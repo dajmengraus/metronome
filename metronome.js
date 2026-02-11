e211b343422ecc42dc414f6a291a4f770e5bfef4
Dajmen Graus <2062141graus@zuyd.nl>
2026-02-11 21:33:24 +0100

    add audio functionality

--- metronome.js
+++ metronome.js
@@ -1,6 +1,10 @@
 const btnStart = document.getElementById("btn-start");
 const btnStop = document.getElementById("btn-stop");
 const beatAccent = document.getElementById("beat-accent");
+const beats = document.getElementById("beats");
+
+const beatAudio = new Audio("audio/beat.mp3")
+const beatAccentAudio = new Audio("audio/beat-accent.mp3")
 
 let bpm = 100;
 let beatsPerBar = 4;
@@ -11,15 +15,19 @@ let intervalId = null;
 const interval = (60 / bpm) * 1000;
 
 function start() {
+    for (let beat of beats.children) {
+        console.log(beat);
+    }
     if (intervalId !== null) return;
 
     intervalId = setInterval(() => {
         beat = (beat % beatsPerBar) + 1;
         if (beat === 1) {
-            //accented beat
-            console.log("TICK");
+            beatAccentAudio.currentTime = 0;
+            beatAccentAudio.play()
         } else {
-            console.log("tick");
+            beatAudio.currentTime = 0;
+            beatAudio.play();
         }
     }, interval)
 }
